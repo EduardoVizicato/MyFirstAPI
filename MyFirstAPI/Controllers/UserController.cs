8e4663094557f3caa4dca85ebeb553dbca86d6a1
EduardoVizicato <eduardovizicato@gmail.com>
2024-03-21 14:47:26 -0300

    Finalização do projeto

--- MyFirstAPI/Controllers/UserController.cs
+++ MyFirstAPI/Controllers/UserController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Data.Map;
 using MyFirstAPI.Models;
+using MyFirstAPI.Repository.Interfaces;
+using System.Runtime.InteropServices;
 
 namespace MyFirstAPI.Controllers
 {
@@ -8,10 +11,47 @@ namespace MyFirstAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult <List<UserModel>> SearchUser()
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
         {
+            _userRepository = userRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> SearchUser()
+        {
+            List<UserModel> users = await _userRepository.GetAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> SearchUserById(int id)
+        {
+            UserModel user = await _userRepository.GetUserByID(id);
+            return Ok(user);
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<UserModel>> AddUser([FromBody] UserModel userModel)
+        {
+            UserModel user = await _userRepository.AddUser(userModel);
+
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>> UpdateUser([FromBody] UserModel userModel, int id)
+        {
+            userModel.Id = id;
+            UserModel user = await _userRepository.UpdateUser(userModel, id);
             return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<UserModel>> DeleteUser([FromBody] int id)
+        {
+            bool deleted = await _userRepository.DeleteUser(id);
+            return Ok(deleted);
         }
     }
 }
